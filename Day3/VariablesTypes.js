489d6a33d1a5b2d1230b68e9fb01739a20c1966c
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-06 16:46:25 +0530

    Pushing file into git

---
+++ Day3/VariablesTypes.js
@@ -0,0 +1,40 @@
+//String 
+
+let str1 = "Chaithu";
+console.log(str1 , typeof(str1));
+
+let str2  = 'Sai';
+console.log(str2 , typeof(str2));
+
+let str3 = `chaitanay`;
+console.log(str3 , typeof(str3));
+
+//Boolean
+
+let bool1 = true;
+console.log(bool1, typeof(bool1));
+
+let bool2 = false;
+console.log(bool2 , typeof(bool2));
+
+//number
+
+let num1 = 10;
+console.log(num1 , typeof(num1));
+let num2 = 10.2345;
+console.log(num2 , typeof(num2));
+let num3 = 10e3;
+console.log(num3 , typeof(num3));
+
+//undefined
+
+let a;
+console.log(a , typeof(a));
+
+//null
+
+let x = null;
+console.log(x , typeof(x));
+
+
+
