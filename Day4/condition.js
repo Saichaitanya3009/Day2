877dcd502f54634572840872c2b6398287e0f07d
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-07 18:43:38 +0530

    Push

---
+++ Day4/condition.js
@@ -0,0 +1,11 @@
+let value = prompt("Enter the value");
+if(value == 20){
+    console.log("It is not greatest number");
+}else if(value == 30){
+    console.log("It is not greatest number");
+}else if(value == 40){
+    console.log("Greates number");
+}
+else{
+    console.log("Given number out of the range");
+}
---
+++ Day4/switch.js
@@ -0,0 +1,15 @@
+let value = Number(prompt("Enter a value"));
+switch (value) {
+    case 20:
+        console.log("Not greatest number");
+        break;
+        case 30:
+        console.log("Not greatest number");
+        break;
+        case 40:
+        console.log("Greatest number");
+        break;
+    default:console.log("Given number out of the range");
+        break;
+}
+
