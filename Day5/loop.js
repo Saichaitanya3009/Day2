3b90397efd6f5ddaa63f46ab818e72b7cf1d3f37
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-08 13:19:10 +0530

    Pushing the files

---
+++ Day5/loop.js
@@ -0,0 +1,39 @@
+for(let i=1; i<=5;i++){
+    console.log("Chaithu")
+}
+
+// Code Written by me.
+for(let i=1; i<=100;i++){
+    if(i%2==0){
+        console.log(i+"Even")
+    }else
+        console.log(i+"Odd")
+    // console.log(i);
+}
+
+let m = 1;
+let n = prompt("Enter the value");
+    for(i=1;i<=n;i++){
+        console.log(n);
+        m = m*1;
+    }
+
+// While Loop
+// ***********
+// let i = 1;
+// while(i >= 5;i++){
+//     console.log("Plz don't talk with me")
+// }
+
+let i = 1;
+do
+{
+    i++;
+    console.log("Hello");
+}while(i>=5);
+
+function addToCart(){
+    alert("Product addes");
+    document.body.style.background="red";
+}
+addToCart()
