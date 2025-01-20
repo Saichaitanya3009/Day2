a201d0f5767afba800351dd8aaf767591435e02b
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-20 19:37:43 +0530

    new files

---
+++ Day8/array.js
@@ -0,0 +1,58 @@
+// let user1 = "Chaithu";
+// let user2 = "Sai";
+// let user3 = "Raghav";
+// let user4 = "Varshith";
+// let user5 = "Sumanth";
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+
+let user = ["Sai","Chaithu","Varshith","Sumanth"];
+
+// console.log(user[0]);
+// console.log(user[1]);
+// console.log(user[2]);
+// console.log(user[3]);
+
+// for(let i=0;i<=user.length;i++);
+// {
+//     console.log(user[i]);   
+// }
+
+//##MapMethod//
+
+// user.map((user , i)=>{
+//     console.log(user , i);
+// })
+
+// let x = user.map((user , i)=> {
+//     return user; //ExplicitReturn
+// });
+// console.log(x);
+
+// let x = user.forEach((user , i)=>{
+//     //console.log(user);
+//     return user;
+// });
+// console.log(x);
+
+//object//
+let userDetails = {
+    name : "Chaithu",
+    Age : 20,
+    address : {
+        city : "Ananthapur",
+        area : "Gooty",
+        streetNo : 143,
+        Pincode : {
+            pincode1:123456,
+            pincode2:998765
+        }
+    }
+   
+
+};
