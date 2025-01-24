380788298974f56de616ff91d013a24449375f35
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-24 23:18:48 +0530

    New Files

---
+++ Day12/Dom.js
@@ -0,0 +1,63 @@
+// let ele = document.getElementsByClassName("test");
+// ele[0].style.backgroundColor="dodgerblue"
+// //console.log(ele);
+// //console.log(Array(ele));//this method used to check whether the arry is pre array.
+
+// //! SPread operator:- This operator is used to take out each and every value from 
+// //!Original array and store it in one more new array (pure array).
+
+// //Syntax of Sread  - [...varaible]
+
+// let x = [...ele];
+// console.log(x , Array.isArray(x));
+// x.map(element=>{
+//     element.style.backgroundColor="tomato";
+//     element.style.color="white";
+//     element.style.textTransform = "uppercase";
+// })
+
+//getLement By Name:
+// let elel = document.getElementsByTagName("div");
+//  [...elel].map(element=>{
+//     console.log(element.innerText);
+//  })
+
+//get quesry selector:
+// let elee = document.querySelector("#demo");
+// console.log(elee);
+// let ele1 = document.querySelector(".test");
+// console.log(ele1);
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+// let demo =document.getElementById("demo");
+// demo.addEventListener("click",()=>{
+//     // console.log("button clicked");
+//     // alert("hi")
+//     document.body.style.backgroundColor="orange"
+// });
+
+// let demo =document.getElementById("demo");
+
+// demo.addEventListener("dblclick",()=>{
+//     console.log("double click");
+// })
+
+// let demo =document.getElementById("demo");
+
+// demo.addEventListener("mouseover",()=>{
+//     document.body.style.backgroundColor="red";
+// })
+
+// demo.addEventListener("mouseleave",()=>{
+//     document.body.style.backgroundColor="teal";
+// })
+
+
+// function changeColor(buttonId, color) {
+//     document.getElementById(buttonId).style.backgroundColor = color;
+// }
+
+// function resetColor(buttonId, originalColor) {
+//     document.getElementById(buttonId).style.backgroundColor = originalColor;
+// }
