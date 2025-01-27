f1ee8139e81cb5c2d70d969a2b0992835e1c969c
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-27 21:29:56 +0530

    Files

--- Day13/Dom1.js
+++ Day13/Dom1.js
@@ -1,14 +1,16 @@
 //dynamic creation of element in js//du
 
-let ele = document.createElement("h1");
-ele.innerText = "Dynamic";
-// ele.id = "demo"
-ele.setAttribute("id" , "demo")
-console.log(ele);
-//ele.removeAttribute("id");
-document.body.appendChild(ele);
+// let ele = document.createElement("h1");
+// ele.innerText = "Dynamic";
+// // ele.id = "demo"
+// ele.setAttribute("id" , "demo")
+// console.log(ele);
+// //ele.removeAttribute("id");
+// document.body.appendChild(ele);
 
-let image = document.creatElement("img");
-image.src="./vardhan.jpg";
-console.log(image);
-document.body.appendChild(image)
+// let image = document.creatElement("img");
+// image.src="./vardhan.jpg";
+// console.log(image);
+// document.body.appendChild(image)
+
+
---
+++ Day14/Dom1.js
@@ -0,0 +1,61 @@
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id" , "mainBlock");
+// console.log(mainEle);
+
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class" ,"topBlock");
+
+// let image = document.createElement("img");
+// image.src="https://pixabay.com/illustrations/girl-woman-surreal-night-outdoors-8788687/";
+// image.width="300";
+// image.height="300";
+// image.style.objectFit="cover"
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class" , "bottomBlock");
+
+// let h1 = document.createElement("h1");
+// h1.innerText="Girl";
+
+// let btn = document.createElement("buttom");
+// btn.innerText="View More";
+ 
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+// bottomEle.appendChild(image);
+// bottomEle.appendChild(topEle);
+// bottomEle.appendChild(bottomEle);
+
+// document.body.appendChild(mainEle);
+
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+
+let gender = document.getElementsByName("gender");
+// console.log(gender);
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = "";
+    for(let i = 0;i<=gender.length-1;i++){
+        // console.log(gender[i].value);
+        // console.log(gender[i].checked);
+        if(gender[i].checked == true){
+            // console.log(gender[i].value);
+            gen += gender[i].value;
+        }
+    };
+    let userDetails={
+        username:un,
+        password:up,
+        gender:gen
+
+    };
+    console.log(userDetails);
+    sessionStorage.setItem("userData" ,JSON.stringify(userDetails) )
+})
+
+
