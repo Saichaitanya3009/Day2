3f09faa8b2a7eb2003f9a08a400cca68657d66a4
Saichaitanya3009 <saichaithanyanadikudi3009@gmail.com>
2025-01-28 21:10:41 +0530

    New Files

--- Day14/Dom1.js
+++ Day14/Dom1.js
@@ -31,9 +31,23 @@
 let form = document.querySelector("form");
 let username = document.getElementById("uName");
 let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
 
 let gender = document.getElementsByName("gender");
 // console.log(gender);
+
+
+//!hide and showing the password
+check.addEventListener("click" , (event)=>{
+    if(event.target.checked == true){
+        password.setAttribute("type" , "text");
+        show.innerText = "hide password";
+    }else{
+        password.setAttribute("type" , "password");
+        show.innerText="show password";
+    }
+})
 
 form.addEventListener("submit" , event=>{
     event.preventDefault();
